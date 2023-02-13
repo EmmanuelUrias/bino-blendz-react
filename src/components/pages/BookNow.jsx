28b60e066981e18208dab2c5b62368a5d60b1ae1
EmmanuelU <emmanuelurias60@icloud.com>
2023-02-13 13:14:10 -0800

    added some styling

--- src/components/pages/BookNow.jsx
+++ src/components/pages/BookNow.jsx
@@ -1,7 +1,24 @@
+import InstagramEmbed from "../InstagramEmbedCode"
+import '../BookNowPage.css'
 
 function BookNow() {
+
+  
   return (
-    <div>BookNow</div>
+    <div className="book-page">
+      <div className="btn-section">
+        <div className="btn-container">
+          <h2 className="btn-text"></h2>
+          <button className="booking-link-btn">
+           Book On Our Instagram
+
+          </button>
+        </div>
+      </div>
+      <div className="bottom-third">
+        {/* <InstagramEmbed /> */}
+    </div>
+    </div>
   )
 }
 
